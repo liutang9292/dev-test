430910a034d12547e8a607f5645a763110f56691
liutang9292 <929296172@qq.com>
2020-09-06 12:12:30 +0800

    first commit

---
+++ src/main/java/com/tl/test1_1/Test11Application.java
@@ -0,0 +1,13 @@
+package com.tl.test1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tl/test1_1/Test11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl.test1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
